591e893cbcc7bed50760afff67b8a3f804b2f127
ZhengRaymond <raymond.zheng@edu.uwaterloo.ca>
2017-09-08 23:44:45 -0400

    Initial paging/project structure.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Affix from 'antd/lib/affix';
+import BlipNav from './containers/blipNav';
+import BlipHeader from './containers/blipHeader';
+import BlipBody from './containers/blipBody';
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Affix><BlipNav /></Affix>
+        <BlipHeader />
+        <BlipBody />
       </div>
     );
   }
